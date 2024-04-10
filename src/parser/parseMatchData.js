96ff9eeb8fc044d21abfba769051ccb8cc2ca316
LucasMig <lucasmigliori@gmail.com>
2024-04-10 18:32:42 -0300

    feat: created parser's basic functionality, which solves the first task

---
+++ src/parser/parseMatchData.js
@@ -0,0 +1,40 @@
+import {
+  extractKillData,
+  extractPlayerName,
+  newGame,
+} from "../settings/utils.js";
+import { processKillEvent } from "./processKillEvent.js";
+
+export function parseMatchData(lines) {
+  const matches = {};
+  let currentMatchData = null;
+  let matchCount = 1;
+
+  lines.forEach((line) => {
+    if (line.includes("ClientUserinfoChanged:")) {
+      const playerName = extractPlayerName(line);
+      currentMatchData.players.push(playerName);
+    }
+
+    if (line.includes("Kill:")) {
+      const killData = extractKillData(line);
+      currentMatchData = processKillEvent(currentMatchData, killData);
+    }
+
+    if (
+      line.includes("ShutdownGame:") ||
+      (currentMatchData && line.includes("InitGame:"))
+    ) {
+      currentMatchData.players = [...new Set(currentMatchData.players)];
+      matches[`game_ ${matchCount}`] = currentMatchData;
+      matchCount++;
+      currentMatchData = null;
+    }
+
+    if (!currentMatchData && line.includes("InitGame:")) {
+      currentMatchData = newGame();
+    }
+  });
+
+  return matches;
+}
